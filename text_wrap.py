bd09a2879e4993ece20537171172a614c9ecb85d
Victor Bespalko <v1ct0r@yandex.ru>
2019-04-17 21:15:41 +0300

    Update text_wrap.py
    Modified to create SQL queries which I use often.

--- text_wrap.py
+++ text_wrap.py
@@ -1,18 +1,18 @@
 
 # Column we want to wrap by Prefix and Postfix
 text = '''
-asas
-aqwqe
-sadasd
+aaa1
+bbb2
+vvv3
 '''
 
 # <list> of strings to process them 1 by 1
 strings_one_by_one = text[1:-1].split('\n')
 
 # Prefix and Postfix
-prefix = 'ama mighty prefix!'
-postfix = 'ama da postfix11!'
+prefix = r"INSERT INTO tags_approved_values VALUES ('"
+postfix = r"', 'contact');"
 
 # Print nice processed column
 for str in strings_one_by_one:
-    print(prefix, str, postfix)
+    print(prefix+str+postfix)
