403a8f093df69293559a8c44d62a119f8110ccf0
Victor Bespalko <v1ct0r@yandex.ru>
2019-05-20 14:25:47 +0300

    Update text_deduplicate.py

--- text_deduplicate.py
+++ text_deduplicate.py
@@ -1,26 +1,8 @@
 # Column we want to exlude duplicate entries
 text = '''
-seankane@upwork.com 
-scrum-program-management@upwork.com
-seankane@upwork.com 
-client-experience@upwork.com
-iam-eng@upwork.com
-reports@upwork.com
-reporting-backend@upwork.com
-scrum-work-management@upwork.com
-wms-agora@upwork.com
-sre@upwork.com
-dba@upwork.com
-infrastructure@upwork.com
-dps@upwork.com
-Old Team Name Not required
-Old Team Name Not required
-scrum-api@upwork.com
-tnseng@upwork.com
-Old Team Name Not required
-reporting-backend@upwork.com
-marketing-engineering-team@upwork.com
-marketing-engineering-team@upwork.com
+aaa
+aaa
+bbb
 '''
 
 # <list> of strings to process them 1 by 1
